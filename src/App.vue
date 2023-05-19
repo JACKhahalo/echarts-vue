b6dd68638c50fd38585d8c3a0e1a583ed75a6060
kevin <745337567@qq.com>
2023-05-19 22:25:59 +0800

    kevin@0519@1

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,12 @@ import Home from './components/home/home.vue';
 export default {
   components: [Home],
   setup() {
-    const init = () => {
-      axios
-        .get('http://192.168.200.203:8083/prod-api/dbapi/api/select-all-data')
-        .then((res) => {});
-    };
-    init();
+    // const init = () => {
+    //   axios
+    //     .get('http://192.168.200.203:8083/prod-api/dbapi/api/select-all-data')
+    //     .then((res) => {});
+    // };
+    // init();
     return {};
   },
 };
