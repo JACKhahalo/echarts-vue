6df8f53989404765ad07228c4892c34bcfd4afed
liguantong <codertg@163.com>
2023-05-20 15:58:29 +0800

    333

--- src/components/tables-kevin/table6.vue
+++ src/components/tables-kevin/table6.vue
@@ -1,110 +1,152 @@
 <template>
   <div class="row">
-    <span v-for="(item, index) in topList" :key="index" :title="`${item.name}:${item.num}，占比 ${item.percent}`"
-      class="grid topgrid" :style="{ background: item.color }">
+    <span
+      v-for="(item, index) in topList"
+      :key="index"
+      :title="`${item.name}:${item.num}，占比 ${item.percent}`"
+      class="grid topgrid"
+      :style="{ background: item.color }"
+    >
     </span>
     <div class="linetop">
-      <div class="tri" style="border-left: 2px solid #00B0F0;">
-        <div class="trapezoid" style="border-bottom: 2px solid #00B0F0; color: #00B0F0;">范围一：直接排放 2,730tCO2</div>
+      <div class="tri" style="border-left: 2px solid #00b0f0">
+        <div
+          class="trapezoid"
+          style="border-bottom: 2px solid #00b0f0; color: #00b0f0"
+        >
+          范围一：直接排放 {{ resData[0].ct }}tCO2
+        </div>
       </div>
     </div>
-    <span v-for="(item, index) in middleList" :key="index" :title="`${item.name}:${item.num}，占比 ${item.percent}`"
-      class="grid middlegrid" :style="{ background: item.color }" />
+    <span
+      v-for="(item, index) in middleList"
+      :key="index"
+      :title="`${item.name}:${item.num}，占比 ${item.percent}`"
+      class="grid middlegrid"
+      :style="{ background: item.color }"
+    />
     <div class="linemiddle">
-      <div class="tri" style="border-left: 2px solid #0070C0;">
-        <div class="trapezoid" style="border-bottom: 2px solid #0070C0; color: #0070C0;">范围二：供热/电力间接排放105,068tCO2</div>
+      <div class="tri" style="border-left: 2px solid #0070c0">
+        <div
+          class="trapezoid"
+          style="border-bottom: 2px solid #0070c0; color: #0070c0"
+        >
+          范围二：供热/电力间接排放{{ resData[1].ct }}tCO2
+        </div>
       </div>
     </div>
-    <span v-for="(item, index) in bottomList" :key="index" :title="`${item.name}:${item.num}，占比 ${item.percent}`"
-      class=" grid Bottomgrid" :style="{ background: item.color }" />
+    <span
+      v-for="(item, index) in bottomList"
+      :key="index"
+      :title="`${item.name}:${item.num}，占比 ${item.percent}`"
+      class="grid Bottomgrid"
+      :style="{ background: item.color }"
+    />
     <div class="linebottom">
-      <div class="tri" style="border-left: 2px solid #124E7A;">
-        <div class="trapezoid" style="border-bottom: 2px solid #124E7A; color: #124E7A;"> 范围三：价值链间接排放195.6tCO2 </div>
+      <div class="tri" style="border-left: 2px solid #124e7a">
+        <div
+          class="trapezoid"
+          style="border-bottom: 2px solid #124e7a; color: #124e7a"
+        >
+          范围三：价值链间接排放{{ resData[1].ct }}tCO2
+        </div>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
+import hyRequest from "../../request";
 export default {
-  name: 'HeatmapChart',
+  name: "HeatmapChart",
   data() {
     return {
       gridData: [],
       total: 0,
       bottomList: [],
       middleList: [],
-      topList: []
-    }
+      topList: [],
+      resData: [],
+    };
+  },
+  created() {
+    hyRequest
+      .get({
+        url: "/emission/co2",
+      })
+      .then((res) => {
+        this.resData = res.data;
+        console.log(this.resData);
+      });
+  },
+  mounted() {
+    this.initGrid();
+    this.renderGrid();
   },
 
-  mounted() {
-    this.initGrid()
-    this.renderGrid()
-  },
   methods: {
     initGrid() {
       for (let i = 0; i < 100; i++) {
         this.gridData.push({
-          color: '#fff'
-        })
+          color: "#fff",
+        });
       }
     },
     renderGrid() {
       // 数据
-      const data = [{
-        name: 'CH₄排放量（t）',
-        num: 310,
-        percent: 20,
-        color: '#00B0F0'
-      },
-      {
-        name: 'CO₂排放总量（t）',
-        num: 2360.013,
-        percent: 50,
-        color: '#0070C0'
-      },
-      {
-        name: 'N₂O排放量（t）',
-        num: 60,
-        percent: 30,
-        color: '#124E7A'
-      }
-      ]
+      const data = [
+        {
+          name: "CH₄排放量（t）",
+          num: 310,
+          percent: 20,
+          color: "#00B0F0",
+        },
+        {
+          name: "CO₂排放总量（t）",
+          num: 2360.013,
+          percent: 50,
+          color: "#0070C0",
+        },
+        {
+          name: "N₂O排放量（t）",
+          num: 60,
+          percent: 30,
+          color: "#124E7A",
+        },
+      ];
       /* data.sort((a, b) => b.percent - a.percent) // 按照占比从高到低排序 */
-      this.total = data.reduce((acc, cur) => acc + cur.percent, 0)
-      let startIndex = 0
-      let rowIndex = 0
+      this.total = data.reduce((acc, cur) => acc + cur.percent, 0);
+      let startIndex = 0;
+      let rowIndex = 0;
       for (let i = 0; i < data.length; i++) {
-        const item = data[i]
+        const item = data[i];
         const endIndex = Math.round(
           startIndex + (item.percent / this.total) * 100
-        ) // 根据总宽度 100% 计算每个区间的结束位置
+        ); // 根据总宽度 100% 计算每个区间的结束位置
         for (let j = startIndex; j < endIndex; j++) {
-          this.gridData[j].color = data[i].color
-          this.gridData[j].name = item.name
-          this.gridData[j].percent = item.percent + '%'
-          this.gridData[j].num = item.num
+          this.gridData[j].color = data[i].color;
+          this.gridData[j].name = item.name;
+          this.gridData[j].percent = item.percent + "%";
+          this.gridData[j].num = item.num;
         }
         if (endIndex >= (rowIndex + 1) * 10) {
-          rowIndex++
+          rowIndex++;
         }
-        startIndex = endIndex
+        startIndex = endIndex;
       }
 
-      this.topList = this.gridData.filter(v => {
-        return v.percent == '20%'
-      })
-      this.middleList = this.gridData.filter(v => {
-        return v.percent == '50%'
-      })
-      this.bottomList = this.gridData.filter(v => {
-        return v.percent == '30%'
-      })
+      this.topList = this.gridData.filter((v) => {
+        return v.percent == "20%";
+      });
+      this.middleList = this.gridData.filter((v) => {
+        return v.percent == "50%";
+      });
+      this.bottomList = this.gridData.filter((v) => {
+        return v.percent == "30%";
+      });
     },
-  }
-}
+  },
+};
 </script>
 
 <style>
